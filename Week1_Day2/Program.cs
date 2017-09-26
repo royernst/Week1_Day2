fb8273f5c57bed21653a940036f4ed5ab39d39b7
royernst <roy.e31@hotmail.com>
2017-09-26 14:19:11 -0400

    Practiced with data types

--- Week1_Day2/Program.cs
+++ Week1_Day2/Program.cs
@@ -10,6 +10,24 @@ namespace Week1_Day2
     {
         static void Main(string[] args)
         {
+            char firstInitial = 'R';
+            Console.WriteLine(firstInitial);
+
+            bool isTired = true;
+            Console.WriteLine(isTired);
+
+            float pi = 3.141592653589753238462643383f;
+            decimal pi2 = 3453.141592653589753238462643383141592653589753238462643383141592653589753238462643383m;
+            double someNumber = 3.141592653589753238462643383d;
+
+            Console.WriteLine(pi);
+            Console.WriteLine(pi2);
+            Console.WriteLine(someNumber);
+
+            int firstNumber = 553;
+            int secondNumber = 948554;
+
+            float quotient = firstNumber / secondNumber;
         }
     }
 }
